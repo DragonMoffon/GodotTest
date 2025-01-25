a9f1d069ff0aea8954e39b967fce810e1a67f288
DragonMoffon <happydog.aja@gmail.com>
2025-01-25 23:43:56 +1300

    few more rhyme groups (we need to sit down and fill this out)

--- data/word.gd
+++ data/word.gd
@@ -2,7 +2,7 @@ class_name Word
 extends Resource
 
 enum Type { NONE = 0, NOUN = 1, VERB = 2, ADJECTIVE = 4, ADVERB = 8, OTHER = 16 }
-enum Rhyme { NONE, AIN, PLE, OKED}
+enum Rhyme { NONE, AIN, PLE, OKED, AD, ICKY, }
 enum Sounds { NONE, ɑː, ɒ, æ, aɪ, aʊ, ɛ, eɪ, ɪ, iː, oʊ, ɔː, ɔɪ, ʊ, uː, ʌ, ə, i, u, əl, ən, əm, b, d, dj, dʒ, ð, f, ɡ, h, hw, j, k, l, lj, m, n, nj, ŋ, p, r, s, sj, ʃ, t, tj, tʃ, θ, θj, v, w, z, zj, ʒ}
 enum Vowel { NONE, ɑː, ɒ, æ, aɪ, aʊ, ɛ, eɪ, ɪ, iː, oʊ, ɔː, ɔɪ, ʊ, uː, ʌ, ə, i, u, əl, ən, əm }
 
